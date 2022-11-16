dcde2cedca6389e9faaf6c25d08bcca4a8a51b0a
Navjot Singh <navjotn406@gmail.com>
2022-11-17 05:35:04 +0600

    Initial Commit
    1. Create the project.
    2. Create Todo model class.

---
+++ Web1001Todo/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace Web1001Todo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime? CompleteDate { get; set; }
+    }
+}
